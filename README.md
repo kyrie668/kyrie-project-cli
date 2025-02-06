582fa176cf3da344978dd3443aa1769fef783725
KyrieYao <yaofh@hinadm.com>
2025-02-06 17:23:47 +0800

    init"kyrie 脚手架初始化"

---
+++ README.md
@@ -0,0 +1,39 @@
+# kyrie-project-cli
+
+A CLI tool to build Vite projects with admin project template and visualization-screen template installed.
+
+## Installation
+
+You can install the CLI globally using npm:
+
+```sh
+npm install -g kyrie-project-cli
+create-kyrie-project my-project
+```
+
+## Example:
+
+```sh
+create-kyrie-project my-project
+
+
+
+cd my-project
+npm install
+npm run dev
+```
+
+# Features
+
+- Vite
+- ShadCN UI
+- TypeScript
+- Tailwind CSS
+
+# Github
+
+https://github.com/kyrie668
+
+# License
+
+MIT © KYRIE668
---
+++ bin/index.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import fs from 'fs-extra';
+import path from 'path';
+import degit from 'degit';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const { version } = require('../package.json');
+
+const program = new Command();
+
+const REPOS = {
+  admin: 'https://github.com/kyrie668/vite-shadcn-ui.git',
+  'visualization-screen': 'https://github.com/kyrie668/visualization-screen.git',
+  //   simple: 'github-user/simple-repo',
+};
+
+async function createProject(projectName) {
+  const projectDir = path.resolve(process.cwd(), projectName);
+
+  // 检查目录是否存在
+  if (fs.existsSync(projectDir)) {
+    const { overwrite } = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'overwrite',
+        message: ` ${chalk.red(projectName)} already exists. Do you want to overwrite it?`,
+        default: false,
+      },
+    ]);
+
+    if (!overwrite) {
+      console.log(chalk.red('❌ cancelled.'));
+      process.exit(1);
+    } else {
+      fs.removeSync(projectDir);
+      console.log(chalk.yellow('⚠️  deleting existing directory...'));
+    }
+  }
+
+  // 选择模块
+  const { moduleChoice } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'moduleChoice',
+      message: 'Please select a module to create your project',
+      choices: Object.keys(REPOS),
+    },
+  ]);
+
+  // 克隆 GitHub 仓库
+  console.log(chalk.blue(`🚀 cloning ${moduleChoice} repository...`));
+  const emitter = degit(REPOS[moduleChoice], { cache: false, force: true });
+  try {
+    await emitter.clone(projectDir);
+
+    // 删除 `.git` 解绑 Git 记录
+    fs.removeSync(path.join(projectDir, '.git'));
+    // 提示下一步操作
+    console.log(chalk.cyan(`\n👉 Next steps:`));
+    console.log(`   cd ${projectName}`);
+    console.log('   npm install');
+    console.log('   npm run dev');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+program
+  .version(version)
+  .argument('<project-name>', 'Directory to create your new project')
+  .action((projectName) => {
+    createProject(projectName);
+  });
+
+program.parse(process.argv);
